086344d51672a592bcf62e12d9d17dddb32a230b
xiaonan39 <18864570316@163.com>
2021-01-19 17:45:22 +0800

    mini-player

---
+++ src/components/mini-player.vue
@@ -0,0 +1,27 @@
+<!--  -->
+<template>
+  <div class="miniplayer"></div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted () {},
+
+  methods: {},
+
+  created () {}
+
+}
+
+</script>
+<style lang='scss' scoped>
+</style>
